9e9374404ceb0e1996c1bae9c03c1070975c180b
Jay <jzawadzki19@gmail.com>
2024-01-22 17:37:33 +0100

    implemented base ui

--- image-processing/UI/image-processing-client/src/app/app.component.ts
+++ image-processing/UI/image-processing-client/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { UploadScreeenComponent } from './upload-screen/upload-screen.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, UploadScreeenComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
