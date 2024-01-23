cf60d18e9f935b552733b205da730e2da6541145
Jay <jzawadzki19@gmail.com>
2024-01-23 20:57:27 +0100

    UI styling

--- image-processing/UI/image-processing-client/src/app/upload-screen/upload-screen.component.ts
+++ image-processing/UI/image-processing-client/src/app/upload-screen/upload-screen.component.ts
@@ -1,23 +1,34 @@
 import { Component } from "@angular/core";
 import { UploadPictureService } from "./upload-picture.service";
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon';
+import { FormControl, ReactiveFormsModule } from "@angular/forms";
 
 @Component({
     selector: 'upload-screen',
     templateUrl: './upload-screen.component.html',
     styleUrl: './upload-screen.component.scss',
-    standalone: true
+    standalone: true,
+    imports: [
+        MatButtonModule,
+        MatCardModule,
+        MatIconModule,
+        ReactiveFormsModule
+    ]
 })
 export class UploadScreeenComponent {
 
     constructor(private imageService: UploadPictureService) {}
 
     private imageFormData: FormData = new FormData();
+    loadedFile: File;
 
     onFileSelect($event: any) {
-        const file: File = $event.target.files[0];
-        if (file) {
-            this.imageFormData.append("image", file);
-            console.log(file);
+        this.loadedFile = $event.target.files[0];
+        if (this.loadedFile) {
+            this.imageFormData.append("image", this.loadedFile);
+            console.log(this.loadedFile);
             this.imageService.sendPicture();
         }
     }
